c4599f07531bca4198815158d147d8ac2dda9d68
Farizph <18221015@std.stei.itb.ac.id>
2023-12-06 00:10:35 +0700

    upload file in pgoress

--- src/app/api/book/route.js
+++ src/app/api/book/route.js
@@ -1,18 +1,5 @@
-import multer from 'multer'
 import Book from "@models/book";
 import { connectToDB } from "@utils/database";
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const destinationPath = '@upload/'
-        cb(null, destinationPath)
-    },
-    filename: (req, file, cb) => {
-        const filename = Date().toISOString()+'-'+file.originalname
-        cb(null, filename)
-    }
-})
-const upload = multer({storage})
 
 export const GET = async (request) => {
     console.log(__dirname)
@@ -28,7 +15,6 @@ export const GET = async (request) => {
     }
 };
 
-handler.use(upload.array("image"))
 
 export const POST = async (request) => {
     const { full_name, phone, email, student_id, check_in, student_card, id_card } = await request.json();
@@ -36,26 +22,19 @@ export const POST = async (request) => {
     try {
         await connectToDB()
 
-        upload.single('file')(req, res, async function (err) {
-            if (err instanceof multer.MulterError) {
-              return res.status(500).json({ error: 'Multer error' });
-            } else if (err) {
-              return res.status(500).json({ error: 'Unknown error' });
-            }
-    
             const newBook = new Book({
                 full_name,
                 phone,
                 email,
                 student_id,
                 check_in,
-                student_card: student_card.name,
+                student_card,
                 id_card: id_card.name,
             });
+            console.log(student_card)
             await newBook.save();
     
             return new Response(JSON.stringify(newBook), { status: 200 })
-          });
     } catch (error) {
         return new Response("Failed to create new book", { status: 500 })
     }
--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react'
 import { useRouter } from "next/navigation";
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
 import axios from 'axios'
 
 interface BookDataType {
@@ -16,6 +18,25 @@ interface BookDataType {
 
 export default function Home() {
     const router = useRouter()
+
+    async function upload(data: FormData) {
+        'use server';
+        const file: File | null = data.get('file') as unknown as File;
+        if (!file) {
+          throw new Error('No file uploaded');
+        }
+    
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+    
+        // With the file data in the buffer, you can do whatever you want with it.
+        // For this, we'll just write it to the filesystem in a new location
+        const path = join('./', 'files', file.name);
+        await writeFile(path, buffer);
+        console.log(`open ${path} to see the uploaded file`);
+    
+        return { success: true };
+      }
 
     const [bookData, setBookData] = useState<BookDataType>({
         full_name: '',
@@ -212,14 +233,19 @@ export default function Home() {
             required
           />
         </div>
-        <div className="text-center">
+
+        {/* <div className="text-center">
           <button
             className="w-[200px] bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none "
             onClick={handleSubmit}
           >
             Book Now!
           </button>
-        </div>
+        </div> */}
+        <form action={upload}>
+            <input type="file" name="file" />
+            <input type="submit" value="Upload" />
+        </form>
       </div>
         
       </div>
